21d35c4bdde785b0d9042a6046359bd9234e3e76
Georgia <ghobs987@gmail.com>
2021-12-16 22:58:50 +0000

    will now load

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// const ApiRoutes = require("./public/api.js")
 app.use(express.static("public"));
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
