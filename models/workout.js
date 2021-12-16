138170a8497125e7b0ba5f2d8f3e1931898114ba
Georgia <ghobs987@gmail.com>
2021-12-16 22:20:53 +0000

    added  server js, schema, and p.json update and git ignore

---
+++ models/workout.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+        type: {
+            type: String,
+            required: "type is required"
+        },
+        name: {
+            type: String,
+            required: "name is required"
+        },
+        duration: {
+            type: Number,
+            required: "duration is required"
+        },
+        distance: Number,
+        reps: Number,
+        sets: Number,
+        weight: Number,
+
+    },
+  ],
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+
+module.exports = workout;
