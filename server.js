25bc876956ffa768d0c1e6009f93bf79d075a3ae
Georgia <ghobs987@gmail.com>
2021-12-17 23:58:14 +0000

    deploying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = require("./routes")
+const router = require("./routes");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,14 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // const ApiRoutes = require("./public/api.js")
-app.use(express.static("public", {extensions: ["html"]}));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+app.use(express.static("public", { extensions: ["html"] }));
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker",
+  {
+    // useNewUrlParser: true,
+    // useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
-// app.use("/", redirects )// 
+// app.use("/", redirects )//
 app.use(router);
 
 app.listen(PORT, () => {
